edebf8d9095d30df69986fd2a291c7cdf66b2d96
Hernan Pinochet <hernan.pinochet@usach.cl>
2021-07-08 23:03:35 -0400

    Se finaliza pagina de registro y se agregan componenetes de pagina de inicio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import Operaciones from './Operaciones.js'
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Encabezado from './components/Encabezado.js'
+import Footer from './components/Footer.js'
+import Inicio from './components/Inicio.js'
+import Signin from './components/Signin.js'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 function App() {
     return (
+        <Router>
+            <Encabezado />
+            <div className="container">
+                <Route path="/" exact component={Inicio} />
+                <Route path="/registrar" component={Signin} />
+            </div>
+            <br></br><br></br>
+            
+            < Footer /> 
 
-        <div className="App d-flex justify-content-center align-items-center">
-
-            <Operaciones/>
-
-        </div>
+        </Router>
     );
 }
 
