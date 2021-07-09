d1807289739a405d8ac916226524598847eae649
gabustoledo <gabriel.bustamante@usach.cl>
2021-07-08 23:17:38 -0400

    Se realiza vista de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Encabezado from './components/Encabezado.js'
 import Footer from './components/Footer.js'
 import Inicio from './components/Inicio.js'
+import Login from './components/Login.js'
 import Signin from './components/Signin.js'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -15,6 +16,7 @@ function App() {
             <Encabezado />
             <div className="container">
                 <Route path="/" exact component={Inicio} />
+                <Route path="/login" component={Login} />
                 <Route path="/registrar" component={Signin} />
             </div>
             <br></br><br></br>
